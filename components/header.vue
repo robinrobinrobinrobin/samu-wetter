c980e54b9444543609b649e54c7aa3f3d9d4323e
robinrobinrobinrobin <39830658+robinrobinrobinrobin@users.noreply.github.com>
2025-01-10 10:05:39 +0100

    hover + header active page indicator + header theme

--- components/header.vue
+++ components/header.vue
@@ -1,16 +1,38 @@
 <template>
   <header
-    class="px-6 pt-6 pb-4 sticky top-0 bg-black/80 backdrop-blur-lg flex justify-between items-center text-white text-4xl font-bold z-50"
+    class="px-6 pt-6 pb-4 sticky top-0 backdrop-blur-lg flex justify-between items-center text-4xl font-bold z-50"
+    :class="
+      route.path === '/' || route.path.includes('projects')
+        ? 'bg-black text-white'
+        : 'bg-white text-black'
+    "
   >
     <NuxtLink to="/" class="uppercase">Samu Wetter</NuxtLink>
     <nav class="flex gap-6">
-      <NuxtLink to="/" class="uppercase">Projekte</NuxtLink>
-      <NuxtLink to="/prozess" class="uppercase">Prozess</NuxtLink>
-      <NuxtLink to="/ueber-mich" class="uppercase">Über Mich</NuxtLink>
+      <NuxtLink
+        to="/"
+        class="uppercase"
+        :class="{ 'text-yellow': route.path === '/' || route.path.includes('projects') }"
+        >Projekte</NuxtLink
+      >
+      <NuxtLink
+        to="/prozess"
+        class="uppercase"
+        :class="{ 'text-yellow': route.path === '/prozess' }"
+        >Prozess</NuxtLink
+      >
+      <NuxtLink
+        to="/ueber-mich"
+        class="uppercase"
+        :class="{ 'text-yellow': route.path === '/ueber-mich' }"
+        >Über Mich</NuxtLink
+      >
     </nav>
   </header>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const route = useRoute();
+</script>
 
 <style></style>
